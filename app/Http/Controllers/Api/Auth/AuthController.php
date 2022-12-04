ef9f9617b384f86b59eb4e1d89e4c04fb4bfbe98
skyfury2651 <skyfury2651@gmail.com>
2022-12-04 18:01:17 +0700

    feat: login user password and some small service

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Services\UserService;
+use App\Http\Traits\HasResponseApi;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    use HasResponseApi;
+
+    public function login(LoginRequest $request, UserService $userService)
+    {
+        try {
+            $token = $userService->loginUser($request->only(['email', 'password']));
+
+            return $this->apiResponse($token);
+        } catch (\Exception $exception) {
+            return $this->apiResponse(null, $exception->getCode(), $exception->getMessage());
+        }
+    }
+
+    public function register(RegisterRequest $request, UserService $userService)
+    {
+        try {
+            $token = $userService->registerUser($request->only(['email', 'password', 'name']));
+
+            return $this->apiResponse($token);
+        } catch (\Exception $exception) {
+            return $this->apiResponse(null, $exception->getCode(), $exception->getMessage());
+        }
+    }
+}
---
+++ app/Http/Services/UserService.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Services;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserService
+{
+    protected User $model;
+
+    public function __construct(User $user)
+    {
+        $this->model = $user;
+    }
+
+    public function checkExitedEmail(string $email): bool
+    {
+        return $this
+            ->model
+            ->query()
+            ->where('email', $email)
+            ->exists();
+    }
+
+    public function loginUser(array $params) {
+        if (Auth::attempt($params)) {
+            return $this->generateToken();
+        }
+
+        throw new \Exception('Email or password wrong',422);
+    }
+
+    public function registerUser(array $params)
+    {
+       $user = $this->model->create([
+            'name' => $params['name'],
+            'email' => $params['email'],
+            'password' => Hash::make($params['password']),
+        ]);
+
+       $user->assignRole(Role::ROLE_MEMBER);
+
+       Auth::login($user);
+
+       return $this->generateToken();
+    }
+
+    public function generateToken()
+    {
+        if (Auth::check()) {
+            $listRole = Auth::user()->getRoleNames();
+            return Auth::user()->createToken('accessToken', $listRole->toArray());
+        }
+
+        throw new \Exception('No user logged',401);
+    }
+}
