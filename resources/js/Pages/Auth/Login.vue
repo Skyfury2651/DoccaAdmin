ffc09e18931d3254a540abe405026df3c5f75acb
eleline <hoangtuanh.d1@gmail.com>
2022-12-05 15:44:34 +0700

    feat: login and register ui

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -29,62 +29,76 @@ const submit = () => {
     <GuestLayout>
         <Head title="Log in" />
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
-        </div>
+        <div class="auth-form-wrapper px-4 py-5">
+            <a
+                href="#"
+                class="noble-ui-logo d-block mb-2"
+                >Noble<span>UI</span></a
+            >
+            <h5 class="text-muted fw-normal mb-4">
+                Welcome back! Log in to your account.
+            </h5>
+            <form @submit.prevent="submit">
+                <div class="mb-3">
+                    <InputLabel
+                        for="email"
+                        value="Email address"
+                        class="form-label"
+                    />
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
+                    <TextInput
+                        id="email"
+                        type="email"
+                        class="form-control"
+                        v-model="form.email"
+                        required
+                        autofocus
+                        autocomplete="email"
+                        placeholder="Email"
+                    />
 
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
+                    <InputError
+                        class="mt-2"
+                        :message="form.errors.email"
+                    />
+                </div>
+                <div class="mb-3">
+                    <InputLabel
+                        for="password"
+                        value="Password"
+                        class="form-label"
+                    />
 
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
+                    <TextInput
+                        id="password"
+                        type="password"
+                        class="form-control"
+                        v-model="form.password"
+                        required
+                        autocomplete="current-password"
+                        placeholder="Password"
+                    />
 
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="current-password"
-                />
-
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
+                    <InputError
+                        class="mt-2"
+                        :message="form.errors.password"
+                    />
+                </div>
+                <div>
+                    <PrimaryButton
+                        class="btn btn-primary me-2 mb-2 mb-md-0 text-white"
+                        :disabled="form.processing"
+                    >
+                        Log in
+                    </PrimaryButton>
+                </div>
                 <Link
-                    v-if="canResetPassword"
-                    :href="route('password.request')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    :href="route('register')"
+                    class="d-block mt-3 text-muted"
                 >
-                    Forgot your password?
+                    Not a user? Sign up
                 </Link>
-
-                <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
-                </PrimaryButton>
-            </div>
-        </form>
+            </form>
+        </div>
     </GuestLayout>
 </template>
